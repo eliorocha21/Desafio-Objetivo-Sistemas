ae059f85ec202684a408d099930adf7615f3ea70
Elio Rocha <elioneto21@gmail.com>
2024-01-13 03:25:58 -0300

    Criação de método getter e setter e parâmetro data

--- ObjetivoSistemas/produto.class.php
+++ ObjetivoSistemas/produto.class.php
@@ -1,10 +1,25 @@
 <?php
-
+//PONTO DE VENDA PARA O MERCADINHO JWT
 class Produto {
 
     //criação da classe responsável pelo cadastro de produtos
-    
+
     private $nome;
     private $preco;
     private $quantidade;
+
+    /* Criação de método setProduto que configura os atributos do produto com base em um array de dados, enquanto getProduto retorna esses dados.*/
+
+    public function setProduto($data) {
+        $this->nome = $data["nome"];
+        $this->preco = $data["preco"];
+        $this->quantidade = $data["quantidade"];
+    }
+    public function getProduto() {
+        return array(
+            "nome" => $this->nome,
+            "preco" => $this->preco,
+            "quantidade" => $this->quantidade
+        );
+    }
 }
